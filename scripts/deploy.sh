c5b90069398f4fe8d7cf4264b3854c8ad5411d48
Tyler <tylergrinn@gmail.com>
2020-09-24 15:18:13 -0400

    changed method of getting absolute paths

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync --recursive --delete $cwd/../bin $cwd/../package.json $cwd/../package-lock.json tygr@$HOST:tygr-server
+rsync --recursive --delete $TRAVIS_BUILD_DIR/bin $TRAVIS_BUILD_DIR/package.json $TRAVIS_BUILD_DIR/package-lock.json tygr@$HOST:tygr-server
 
-ssh tygr@$HOST 'bash -sl' < $cwd/deploy_helper.sh
+ssh tygr@$HOST 'bash -sl' < $TRAVIS_BUILD_DIR/scripts/deploy_helper.sh
